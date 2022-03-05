a5ea1555d01c5b31a983d15521b07bdac2a90629
Njoki <kinuthiash@gmail.com>
2022-03-05 23:01:26 +0300

    feat:dates - add limits

--- js/script.js
+++ js/script.js
@@ -1,28 +1,13 @@
 //store birth details in variables to use later
-var date = getElementById.value;('date')
+var date = getElementById('date').value;
+
+
+
+
+
+
 var month = getElementById.value;('month')
 var year = getElementById.value;('year')
 
 // function - birth details validation 
-var validateBirthDetails = (date , month, year)=>{
-  if (date <= 0 || d > 31){
-    alert('Please enter a valid date')
-  }
-  else{
-    alert('ok')
-  }
 
-  if (month <= 0 || month > 12){
-    alert('Please enter a valid month')
-  }
-  else{
-    alert('ok')
-  }
-
-  if (year == NaN){
-    alert('Please enter a valid year')
-  }
-  else{
-    alert('ok')
-  }
-}
