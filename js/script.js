614d50416b033819e321dc629ca582610345c398
Njoki <kinuthiash@gmail.com>
2022-03-06 00:41:11 +0300

    fix:edit calculate function

--- js/script.js
+++ js/script.js
@@ -10,10 +10,15 @@ var calculateDay = ()=>{
   var month = getElementById('month').value;
   var year = getElementById('year').value;
 
-  var centuryDigits = parseInt(year.toString().slice(0,2));
-  var yearDigits =  parseInt(year.toString().slice(2,4))
+  var centuryDigits = parseInt(year.toString().slice(0,2));  
+  var yearDigits =  parseInt(year.toString().slice(2,4));   
+
 
   var dayOfWeek = ( ( (centuryDigits/4) -2*centuryDigits-1) + ((5*yearDigits/4) ) + ((26*(month+1)/10)) + day ) % 7;
-  return dayOfWeek;
+  var dateValue = parseInt(dayOfWeek.toString().slice(1));     
 }
+calculateDay();
+
+// 
+
 
