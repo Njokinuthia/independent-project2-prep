c8d55aec22285d980248b9bda71057ebec6f970c
Njoki <kinuthiash@gmail.com>
2022-03-06 00:17:26 +0300

    feat:add calculation function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-//store birth details in variables to use later
+//
+
+
 
 // calculate day 
 
-var calculateDay = (date , month, year)=>{
+var calculateDay = ()=>{
 
   var date = getElementById('date').value;
   var month = getElementById('month').value;
   var year = getElementById('year').value;
 
-  centuryDigits = parseInt(year.toString().slice(0,2));
-  yearDigits =  parseInt(year.toString().slice(2,4))
+  var centuryDigits = parseInt(year.toString().slice(0,2));
+  var yearDigits =  parseInt(year.toString().slice(2,4))
 
   var dayOfWeek = ( ( (centuryDigits/4) -2*centuryDigits-1) + ((5*yearDigits/4) ) + ((26*(month+1)/10)) + day ) % 7;
   return dayOfWeek;
