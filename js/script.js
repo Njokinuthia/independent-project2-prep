1af8d45180531da1c54a6d17b67e6f59b7a6a087
Njoki <kinuthiash@gmail.com>
2022-03-06 02:00:42 +0300

    feat:add gender function

--- js/script.js
+++ js/script.js
@@ -1,24 +1,54 @@
 //
-
+var submit = () => {
+  calculateDay();
+  determineGenderName();
+  giveOutput();
+}
 
 
 // calculate day 
 
-var calculateDay = ()=>{
+var calculateDay = () => {
 
-  var date = getElementById('date').value;
-  var month = getElementById('month').value;
-  var year = getElementById('year').value;
+  var date = getElementById("date").value;
+  var month = getElementById("month").value;
+  var year = getElementById("year").value;
 
-  var centuryDigits = parseInt(year.toString().slice(0,2));  
-  var yearDigits =  parseInt(year.toString().slice(2,4));   
+  var centuryDigits = parseInt(year.toString().slice(0, 2));
+  var yearDigits = parseInt(year.toString().slice(2, 4));
 
 
-  var dayOfWeek = ( ( (centuryDigits/4) -2*centuryDigits-1) + ((5*yearDigits/4) ) + ((26*(month+1)/10)) + day ) % 7;
-  var dateValue = parseInt(dayOfWeek.toString().slice(1));     
+  var dayOfWeek = (((centuryDigits / 4) - 2 * centuryDigits - 1) + ((5 * yearDigits / 4)) + ((26 * (month + 1) / 10)) + date) % 7;
+  var dateIndex = parseInt(dayOfWeek.toString().slice(0, 1));
 }
-calculateDay();
 
-// 
+
+// Determine gender and name
+
+var determineGenderName = () => {
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+  var femaleNames = ["Akosua", "Adwoa", "Abenna", "Akua", "yaa", "Afua", "Ama"]
+
+  var gender = getElementByType("radio").checked;
+
+  if (gender === male) {
+    console.log(maleNames[dateIndex]);
+  }
+  else {
+    femaleNames(femaleNames[dateIndex]);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
