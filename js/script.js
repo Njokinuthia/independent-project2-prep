732c4149b6ab4db66ff1422d93453c1886b27501
Njoki <kinuthiash@gmail.com>
2022-03-06 21:06:14 +0300

    fix:edit calculation functions

--- js/script.js
+++ js/script.js
@@ -1,18 +1,61 @@
-// button.addEventListener("click",()=> {
-//   h3.classList.add("bbb")
+// TEST JS FILE LINK
+// let body = document.getElementById("body");
+//  body.style.backgroundColor ="red";
+
+// NAVIGATE BETWEEN PAGES
+// go to home page
+let home = document.getElementById("home");
+home.addEventListener("click", (event) => {
+  event.preventDefault();
+  pageOne.classList.remove("d-none")
+  pageTwo.classList.add("d-none")
+  pageThree.classList.add("d-none")
+})
+
+// go to forms page
+let getStarted = document.getElementById("getStarted");
+getStarted.addEventListener("click", (event) => {
+  event.preventDefault();
+  pageOne.classList.add("d-none")
+  pageTwo.classList.remove("d-none")
+  pageThree.classList.add("d-none")
+})
+let form = document.getElementById("form");
+form.addEventListener("click", (event) => {
+  event.preventDefault();
+  pageOne.classList.add("d-none")
+  pageTwo.classList.remove("d-none")
+  pageThree.classList.add("d-none")
+})
+
+// go to info page
+let info = document.getElementById("info");
+info.addEventListener("click", (event) => {
+  event.preventDefault();
+  pageOne.classList.add("d-none")
+  pageTwo.classList.add("d-none")
+  pageThree.classList.remove("d-none")
+})
+
+// let calculate = document.getElementById("calculate");
+// calculate.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   pageOne.classList.add("d-none")
+//   pageTwo.classList.add("d-none")
+//   pageThree.classList.remove("d-none")
 // })
 
-//
-var submit = () => {
-  calculateDay();
-  determineGenderName();  
-}
 
+// // give user their answer
 
+let submit = document.getElementById("submit");
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  calculateDay();  
+})
+ 
 // calculate day 
-
 var calculateDay = () => {
-
   var date = document.getElementById("date").value;
   var month = document.getElementById("month").value;
   var year = document.getElementById("year").value;
@@ -23,28 +66,24 @@ var calculateDay = () => {
 
   var dayOfWeek = (((centuryDigits / 4) - 2 * centuryDigits - 1) + ((5 * yearDigits / 4)) + ((26 * (month + 1) / 10)) + date) % 7;
   var dateIndex = parseInt(dayOfWeek.toString().slice(0, 1));
+  determineGenderName(dateIndex);
 }
 
-
 // Determine gender and name
-
-var determineGenderName = () => {
+var determineGenderName = (dateIndex) => {
   var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
   var femaleNames = ["Akosua", "Adwoa", "Abenna", "Akua", "yaa", "Afua", "Ama"]
 
   var gender = document.getElementByType("radio").checked;
-// TODO:FIX ARRAY INDEX
-  if (gender === male) {
+
+  if (gender === "male") {
     console.log("Akan Name: " + maleNames[dateIndex]);  
   }
   else {
     // TODO:WHATS WRONG WITH PLACE HOLDER
-    femaleNames(`Akan Name: ${femaleNames[dateIndex]}`);
+    console.log(`Akan Name: ${femaleNames[dateIndex]}`);
   }
 }
-
-
-
 
 
 
