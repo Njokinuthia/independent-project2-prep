1af90a9790c4a3d98655b42e92febcb01f5c64f3
Njoki <kinuthiash@gmail.com>
2022-03-07 23:43:03 +0300

    fix:edit validating function

--- js/script.js
+++ js/script.js
@@ -3,45 +3,58 @@
 let submit = document.getElementById("submit");
 submit.addEventListener("click", (event) => {
   event.preventDefault();
-  calculateDay();    
-}) 
+  calculateDay();
+})
 
 // calculate day 
 let calculateDay = () => {
-// object of akan names
+  // object of akan names
   let akanNames = [
-    {day:"sunday", male : "Kwasi" , female: "Akosua"},
-    {day:"monday", male : "Kwadwo" , female: "Adwoa"}, 
-    {day:"tuesday", male : "Kwabena" , female: "Abenaa"}, 
-    {day:"wednesday", male : "Kwaku" , female: "Akua"}, 
-    {day:"thursday", male : "Yaw" , female: "Yaa"}, 
-    {day:"friday", male : "Kofi" , female: "Afua"}, 
-    {day:"Saturday", male : "Kwame" , female: "Ama"}, 
+    { day: "sunday", male: "Kwasi", female: "Akosua" },
+    { day: "monday", male: "Kwadwo", female: "Adwoa" },
+    { day: "tuesday", male: "Kwabena", female: "Abenaa" },
+    { day: "wednesday", male: "Kwaku", female: "Akua" },
+    { day: "thursday", male: "Yaw", female: "Yaa" },
+    { day: "friday", male: "Kofi", female: "Afua" },
+    { day: "Saturday", male: "Kwame", female: "Ama" },
   ];
-// get user input
+  // get user input
   let birthDay = document.getElementById("dob").value;
   let gender = document.getElementById("gender").value;
 
   let date = new Date(birthDay);
   let dayOfBirth = date.getDay();
 
-// determine name
+  if(validate(gender,birthDay) ){
+      // determine name
   let akanName;
-  if(gender === "male"){
-    akanName = akanNames[dayOfBirth].male;   
+  if (gender === "male") {
+    akanName = akanNames[dayOfBirth].male;
   }
-  else{
+  else {
     akanName = akanNames[dayOfBirth].female;
   }
 
-// output answer
+  // output answer
   console.log(akanName);
-  document.getElementById("response").innerText = `Your akan name is ${akanName}`;  
+  document.getElementById("response").innerText = `Your akan name is ${akanName}`;
+
+  }
+  else{
+    alert("Please fill in the form.")
+  }
 
 }
 
-  
-  
+// validation
+
+let validate = (gender, dob) => {
+  if (!gender || !dob) return false; 
+  return true;
+}
+
+
+
 
 
 
